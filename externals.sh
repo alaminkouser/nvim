745f04ff08117328957564136e672bab259f0bfd
AL AMIN KOUSER <36203422+alaminkouser@users.noreply.github.com>
2024-03-15 04:03:43 +0600

    Add files via upload

--- externals.sh
+++ externals.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 while IFS= read -r repo; do
-    printf $repo | awk -F"/" "{print $2}"
+    printf $repo
     DIR="$HOME/.local/share/nvim/site/pack/$(printf $repo | awk -F'/' '{print $2}')/opt/$(printf $repo | awk -F'/' '{print $2}')/"
     if [ -d $DIR ]; then
 	    cd $DIR
-	    git pull
+	    git pull --verbose
     else
 	    mkdir -p $DIR
 	    cd $DIR
-	    git clone "https://github.com/$repo.git" .
+	    git clone "https://github.com/$repo.git" . --verbose
     fi
 done < $(dirname "$0")/externals.txt
--- lua/main/init.lua
+++ lua/main/init.lua
@@ -1,6 +1,13 @@
 vim.cmd [[packadd nvim-lspconfig]]
 vim.cmd [[packadd nvim-cmp]]
 vim.cmd [[packadd cmp-nvim-lsp]]
+vim.cmd [[packadd cmp-buffer]]
+vim.cmd [[packadd cmp-path]]
+vim.cmd [[packadd cmp-cmdline]]
+vim.cmd [[packadd vim-vsnip]]
+vim.cmd [[packadd vim-vsnip-integ]]
+vim.cmd [[packadd neovim]]
 
 require("main.lspconfig")
 require("main.cmp")
+require("main.style")
