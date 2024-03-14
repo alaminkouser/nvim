dccb05fec08294103cd8ce4a581d5ba39b1a5c6a
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-03-15 05:12:07 +0600

    setup

--- externals.sh
+++ externals.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 while IFS= read -r repo; do
-    printf $repo
-    DIR="$HOME/.local/share/nvim/site/pack/$(printf $repo | awk -F'/' '{print $2}')/opt/$(printf $repo | awk -F'/' '{print $2}')/"
+    printf "$repo\n"
+    HASH="$(printf $repo | sha512sum | awk '{print $1}')"
+    DIR="$HOME/.local/share/nvim/site/pack/main/opt/$HASH/"
     if [ -d $DIR ]; then
 	    cd $DIR
 	    git pull --verbose
@@ -10,5 +11,8 @@ while IFS= read -r repo; do
 	    mkdir -p $DIR
 	    cd $DIR
 	    git clone "https://github.com/$repo.git" . --verbose
+    fi
+    if ! grep -q -F "vim.cmd [[packadd $HASH]]" "$(dirname "$0")/lua/main/externals.lua"; then
+        printf "vim.cmd [[packadd $HASH]]\n" >> "$(dirname "$0")/lua/main/externals.lua"
     fi
 done < $(dirname "$0")/externals.txt
