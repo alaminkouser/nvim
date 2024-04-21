fecc983edf91e440c8184257eb3ef545cf8c9e98
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-04-21 13:59:36 +0600

    setup nvim

--- lua/main/nvim-cmp.lua
+++ lua/main/nvim-cmp.lua
@@ -4,19 +4,19 @@ cmp.setup({
 	enabled = true,
 	sources = {
 		{
-			name = 'nvim_lsp'
+			name = "nvim_lsp"
 		}
 	},
 	mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
 })
 
-cmp.setup.cmdline({ "/", '?' }, {
+cmp.setup.cmdline({ "/", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
 		{ name = "buffer" }
