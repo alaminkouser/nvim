2f63d9b0a4cef267e131943dd28d97c70a80a4cc
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-04-23 21:51:10 +0600

    setup nvim

--- lua/main/statusline.lua
+++ lua/main/statusline.lua
@@ -81,16 +81,16 @@ local function lsp()
   local info = ""
 
   if count["errors"] ~= 0 then
-    errors = " %#LspDiagnosticsSignError# " .. count["errors"]
+    errors = " E: " .. count["errors"]
   end
   if count["warnings"] ~= 0 then
-    warnings = " %#LspDiagnosticsSignWarning# " .. count["warnings"]
+    warnings = " W: " .. count["warnings"]
   end
   if count["hints"] ~= 0 then
-    hints = " %#LspDiagnosticsSignHint# " .. count["hints"]
+    hints = " H: " .. count["hints"]
   end
   if count["info"] ~= 0 then
-    info = " %#LspDiagnosticsSignInformation# " .. count["info"]
+    info = " I: " .. count["info"]
   end
 
   return errors .. warnings .. hints .. info .. "%#Normal#"
