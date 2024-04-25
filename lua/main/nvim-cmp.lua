023e997b9c7108931e537ba0f51afe56526d4079
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-04-26 02:10:40 +0600

    setup nvim

--- lua/main/nvim-cmp.lua
+++ lua/main/nvim-cmp.lua
@@ -12,7 +12,8 @@ cmp.setup({
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+	  ["<Tab>"] = cmp.mapping.confirm({select = true})
     }),
 })
 
