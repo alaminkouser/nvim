d23965bb6264ab34f59a085f54e1dab6ceb73cfb
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-08-01 01:23:34 +0600

    UPDATE

--- lua/main/style.lua
+++ lua/main/style.lua
@@ -3,6 +3,7 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.wo.relativenumber = true
 vim.opt.cmdheight=0
+vim.opt.mouse = ""
 vim.cmd [[let &stc="%=%{v:relnum?v:relnum:v:lnum} "]]
 vim.cmd [[set colorcolumn=80]]
 vim.cmd [[set cmdheight=0]]
